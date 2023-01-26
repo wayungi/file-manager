6af410dbc77d31c119a1d1440041a174a9bf9e44
wayungi <fwayungi@gmail.com>
2023-01-26 20:37:20 +0300

    ADD: header icons

--- src/components/partials/header/Header.js
+++ src/components/partials/header/Header.js
@@ -1,7 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { FiBell } from 'react-icons/fi';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
+import { RiGridFill } from 'react-icons/ri';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { CiSearch } from 'react-icons/ci';
 
 const Header = () => (
-  <div>Header</div>
+  <header>
+    <div>
+      <div><RiGridFill /></div>
+      <div><RxHamburgerMenu /></div>
+      <form>
+        <div>
+          <CiSearch />
+          <input type="text" name="seacrh" />
+        </div>
+      </form>
+      <div><AiOutlineQuestionCircle /></div>
+      <div><FiBell /></div>
+    </div>
+  </header>
 );
 
 export default Header;
